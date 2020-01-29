f518be9f9d5bc04295e742e27c00e0ab540c7628
richard dinh <richardndinh@gmail.com>
2020-01-29 00:21:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  add_element_to_end_of_array.push(4, "arrays")
+  array = ["I", "am", "really", "learning"]
+  element = "array!"
 end
 
 def add_element_to_start_of_array(array, element)
