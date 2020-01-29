8a32074fd49b06d0bb5cd84c54235bef997cdd52
richard dinh <richardndinh@gmail.com>
2020-01-29 00:17:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,6 +10,7 @@ end
 def add_element_to_end_of_array(array, element)
   array = ["wow", "I", "am", "really", "learning"]
   elements = "arrays!"
+  add_element_to_end_of_array.push(5, "arrays")
 end
 
 def add_element_to_start_of_array(array, element)
